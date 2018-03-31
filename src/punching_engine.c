543641c4d77e0beb815a95e3653b88844838af44
Isaac <isaacmorneau@gmail.com>
2018-03-31 11:12:42 -0700

    added v1 of reducing the spam after connection is established

--- src/punching_engine.c
+++ src/punching_engine.c
@@ -2,37 +2,67 @@
 #include <limits.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "punching_engine.h"
 #include "wrappers/wrapper.h"
 
-#define BUFF 1024
+#define BUFF 512
 
+typedef struct {
+    int16_t out_port;
+    int16_t in_port;
+} punch_packet;
 
 void start_punch(const char * host, const int port, const int range) {
     int efd = make_epoll();
     struct epoll_event * events = make_epoll_events();
 
     struct sockaddr_storage * addresses = (struct sockaddr_storage*)calloc(range, sizeof(struct sockaddr_storage));
-    int n, * sockets = calloc(range, sizeof(int));
+    int n = 0, offset = 0;
+    int * sockets = calloc(range, sizeof(int));
 
-    char buffer[BUFF];
+
+    //fill out the addresses we are going to try and send to
     for (int i = 0; i < range; ++i) {
         make_storage(addresses + i, host, port + i);
         sockets[i] = make_bound_udp(port + i);
         add_epoll_fd_flags(efd, sockets[i], EVENT_ONLY_IN);
     }
+
+    punch_packet inpack = {0}, outpack = {0};
+
 try:
-    strncpy(buffer, "message", BUFF);
+    if (inpack.out_port) {
+        //we got a message telling us what worked
+        outpack.in_port = inpack.out_port;
+    }
     for (int i = 0; i < range; ++i) {
-        send_message(sockets[i], buffer, strlen(buffer), addresses + i);
+        outpack.out_port = port + i;
+        send_message(sockets[i], (char *)&outpack, sizeof(punch_packet), addresses + i);
     }
     n = wait_epoll_timeout(efd, events, 1000);
     for (int i = 0; i < n; ++i) {
-        memset(buffer, 0, BUFF);
         if (EVENT_IN(events, i)) {
-            read_message(EVENT_FD(events, i), buffer, BUFF);
+            read_message(EVENT_FD(events, i), (char *)&inpack, sizeof(punch_packet));
+            printf("recived op %d ip %d\n", inpack.out_port, inpack.in_port);
+            if (inpack.in_port) {
+                offset = inpack.in_port - port;
+                goto maintain;
+            }
         }
-        printf("recived %s\n", buffer);
     }
     goto try;
+maintain:
+    send_message(sockets[offset], (char *)&outpack, sizeof(punch_packet), addresses + offset);
+    n = wait_epoll_timeout(efd, events, 1000);
+    for (int i = 0; i < n; ++i) {
+        if (EVENT_IN(events, i)) {
+            read_message(EVENT_FD(events, i), (char *)&inpack, sizeof(punch_packet));
+            printf("connection betweeen op %d ip %d steady\n", inpack.out_port, inpack.in_port);
+            if (inpack.in_port) {
+                goto maintain;
+            }
+        }
+    }
+    goto maintain;
 }
